00dc84678b2c9645afd556be23d60e378d58bc88
Cesar Francisco <princexcesar@gmail.com>
2021-04-24 09:38:23 +0800

    fixing some bugs on api

--- user_guide/js/script.js
+++ user_guide/js/script.js
@@ -1,4 +1,4 @@
-$.get("http://pokeapi.co/api/v2/pokemon/?limit=151&offset=0", function(res) {
+$.get("https://pokeapi.co/api/v2/pokemon/?limit=151&offset=0", function(res) {
     for(var i = 0; i < res.results.length; i++) {
         $.get(res.results[i].url, function(images) {
             $('#bulbasaur').append("<div class='box'>"+images.name+"<img src=\""+images.sprites.front_shiny+"\"><div>");
